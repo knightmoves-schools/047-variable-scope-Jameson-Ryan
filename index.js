87623ea004107c8f7c4432277e3b9d7c9ea60b55
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-11 13:56:55 -0600

    Add estimated arrival time calculation

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let distanceInMiles = 100;
 
+function estimatedArrivalTime() {
+  let milesPerHour = 50;
+  return distanceInMiles / milesPerHour;
+}
+
+document.getElementById('result').innerHTML = estimatedArrivalTime;
